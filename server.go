537acbc0f2d1bbc644c64a777e353aba1b246624
cnam <cnam812@gmail.com>
2015-05-05 12:44:30 +0300

    version by gorrila and ping pong

--- server.go
+++ server.go
@@ -2,26 +2,25 @@ package main
 
 import (
 	"net/http"
-	"golang.org/x/net/websocket"
+	//"golang.org/x/net/websocket"
+	"encoding/json"
 	"log"
-	"html/template"
-	"os"
-	//"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin"
+	"github.com/gorilla/websocket"
 )
 
 const (
-	listenAddr = ":9876" // server address
+	listenAddr = "localhost:9876" // server address
+	pingMsgMeta = "system.ping"
+	pingMsgData = "ping"
 )
 
 var (
-	pwd, _ = os.Getwd()
-	RootTemp = template.Must(template.ParseFiles(pwd + "/chat.html"))
-	JSON = websocket.JSON // codec for JSON
 	ActiveClients = make(map[ClientConn]int) // map containing clients
 )
 
 type ClientConn struct {
-	websocket *websocket.Conn
+	conn *websocket.Conn
 	clientIP  string
 }
 
@@ -30,13 +29,26 @@ type ClientMessageType struct {
 	Name string `json:"username"`
 }
 
-func init() {
-	log.Println("Init server")
-	http.HandleFunc("/", RootHandler)
-	http.Handle("/ws", websocket.Handler(HandlerServer))
+type ClientMessageData struct  {
+	Data ClientMessageType `json:"data"`
+	Meta EventStruct  `json:"meta"`
 }
 
-func HandlerServer(ws *websocket.Conn) {
+type MessageStruct struct {
+	Data string    `json:"data"`
+	Meta EventStruct  `json:"meta"`
+}
+
+type EventStruct struct {
+	Event string `json:"event"`
+}
+
+/*func init() {
+	log.Println("Init server")
+	//http.Handle("/ws", websocket.Handler(WsHandler))
+}*/
+
+/*func WsHandler(ws *websocket.Conn) {
 	var err error
 	var clientMessage ClientMessageType
 
@@ -69,27 +81,92 @@ func HandlerServer(ws *websocket.Conn) {
 			}
 		}
 	}
+}*/
+
+var wsupgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
 }
 
-func RootHandler(w http.ResponseWriter, req *http.Request) {
-	err := RootTemp.Execute(w, listenAddr)
+func wshandler(writer http.ResponseWriter, req *http.Request) {
+	var clientMessageData ClientMessageData
+	var msgStruct MessageStruct
+	var pingMsg = `{"data":"pong","meta":{"event":"system.ping"}}`
+
+	conn, err := wsupgrader.Upgrade(writer, req, nil)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		log.Println("Failed to set websocket upgrade: %+v", err)
+		return
 	}
+
+	client := req.RemoteAddr
+	log.Println("Client connected:", client)
+
+	sockCli := ClientConn{conn, client}
+	ActiveClients[sockCli] = 0
+	log.Println("Number of clients connected ...", len(ActiveClients))
+
+	for {
+		t, msg, err := conn.ReadMessage()
+
+		json.Unmarshal(msg, &msgStruct)
+
+		if err != nil {
+			delete(ActiveClients, sockCli)
+			return
+		}
+
+		log.Printf("%+v", msgStruct)
+
+		if msgStruct.Meta.Event == pingMsgMeta && msgStruct.Data == pingMsgData {
+			log.Println("Write system ping")
+			conn.WriteMessage(t, []byte(pingMsg))
+			continue
+		}
+
+		json.Unmarshal(msg, &clientMessageData)
+
+		log.Printf("%+v", clientMessageData)
+
+		msg, err  = json.Marshal(clientMessageData)
+
+		for cs, _ := range ActiveClients {
+			if err = cs.conn.WriteMessage(t, msg); err != nil {
+				// we could not send the message to a peer
+				log.Println("Could not send message to ", cs.clientIP, err.Error())
+			}
+		}
+	}
+}
+
+func RootHandler(c *gin.Context) {
+	c.HTML(http.StatusOK, "chat.tmpl", gin.H{"host": listenAddr})
 }
 
 func main() {
-	err := http.ListenAndServe(listenAddr, nil)
+	router := gin.Default()
+
+	router.LoadHTMLGlob("templates/*")
+	router.GET("/", RootHandler)
+	router.GET("/ws", func(c *gin.Context){
+		wshandler(c.Writer, c.Request)
+		/*log.Println("Connect to ws")
+
+		s := websocket.Server{Handler: WsHandler, Handshake:
+			func (config *websocket.Config, req *http.Request) (err error) {
+				config.Origin, err = websocket.Origin(config, req)
+				if err == nil && config.Origin == nil {
+					log.Println("null origin")
+				}
+			return err
+		}}
+
+		s.ServeHTTP(c.Writer, c.Request)*/
+	})
+
+	err := router.Run(listenAddr);
+
 	if err != nil {
 		panic("ListenAndServe: " + err.Error())
 	}
-
-	/*values := []byte;
-	router := gin.Default()
-
-	router.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "templates/chat.tmpl", values)
-	})
-
-	router.Run(listenAddr);*/
 }
